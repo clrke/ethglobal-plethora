9ee3d4cd50cb2290098136bd411f89280e53014c
hackeziah <lamadridkevinpaul@gmail.com>
2022-03-27 21:58:34 +0800

    commit changes

--- contracts/Star.sol
+++ contracts/Star.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract Star is ERC1155, Ownable {
@@ -57,9 +57,7 @@ contract Star is ERC1155, Ownable {
     function uri(uint256 typeId) public view override returns (string memory) {
         return
             bytes(baseURI).length > 0
-                ? string(
-                    abi.encodePacked(baseURI, typeId.toString(), baseExtension)
-                )
+                ? string(abi.encodePacked(baseURI, typeId, baseExtension))
                 : '';
     }
 }
